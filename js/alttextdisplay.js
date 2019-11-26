ef3477cd87ba86ed6655cc56761a92363b37f2b7
Sydney <sydney.n.hutchinson@gmail.com>
2019-11-25 21:25:54 -0500

    Completed adding content and ready for submission

--- js/alttextdisplay.js
+++ js/alttextdisplay.js
@@ -1,7 +1,8 @@
 function upDate(previewPic){
  
     var sourceImage = previewPic.getAttribute("src");
-    document.getElementById("image").style.backgroundImage = "url('"+sourceImage+"')";
+    document.getElementById("image").src = sourceImage;
+	document.getElementById("image").style.visibility = "visible";
     var sourceText = previewPic.getAttribute("alt");
     document.getElementById("message").innerHTML = sourceText;
 
